2154a006652d7de165e847036fc9302a3eded9a2
Thiago Moro <thiago.g.moro@gmail.com>
2025-08-11 18:39:57 +0100

    Buscando e mostrando na tela o usuario pesquisado

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,9 @@
-import { UserProps } from "../types/user";
+import type { UserProps } from "../types/user.tsx";
 
 import Search from "../components/Search"
 
 import { useState } from "react"
+import User from "../components/User.tsx";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
@@ -11,12 +12,23 @@ const Home = () => {
     const res = await fetch(`https://api.github.com/users/${userName}`)
     const data = await res.json()
 
-    console.log(data)
+    const {avatar_url, login, location, followers, following} = data
+
+    const userData: UserProps = {
+      avatar_url, 
+      login, 
+      location, 
+      followers, 
+      following,
+    }
+
+    setUser(userData);
   }
 
   return (
     <div>
       <Search loadUser={loadUser} />
+      {user && <User {...user}/>}
     </div>
   )
 }
